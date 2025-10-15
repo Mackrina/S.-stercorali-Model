7cc2eb6fb03642b8658181457cab364446cd44f1
Mackrina Winslow <148019291+Mackrina@users.noreply.github.com>
2025-10-15 14:22:29 +1100

    Update Model_NZ.py
    editM_NZ

--- Model_NZ.py
+++ Model_NZ.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def model(y, t, params):
-    # Unpacking the state variables from the input
+    # Variables
     S_C = y[0] # Susceptible Children
     E_C = y[1] # Exposed Children
     I_C = y[2] # Infectious Children
@@ -10,18 +10,18 @@ def model(y, t, params):
     E_A = y[4] # Exposed Adults
     I_A = y[5] # Infectious Adults
     S_D = y[6] # Susceptible Dogs
-    E_DB = y[7] # Non-Zoonotic Exposed Dogs
-    I_DB = y[8] #Non-Zoonotic Infectious Dogs
-    L_Z = y[9] # Zoonotic Infective Larvae in the Environment
-    L_NZ = y[10] # Non-Zoonotic Infective Larvae in the Environment
+    E_DB = y[7] # Exposed Dogs with Lineage B
+    I_DB = y[8] # Infectious Dogs with Lineage B
+    L_A = y[9] # Lineage A larvae
+    L_B = y[10] # Lineage B larvae 
    
 
-    # Unpacking parameters from the input dictionary
+    # parameters 
     b_H = params['b_H'] # Human birth rate (day^(-1))
     d_H = params['d_H'] # Human mortality rate (day^(-1))
     a = params['a'] # Ageing rate from children to adults (day^(-1))
-    beta_C = params['beta_C'] # Effective transmission rate of children (day^(-1))
-    beta_A = params['beta_A'] # Effective transmission rate of adult (day^(-1))
+    beta_C = params['beta_C'] # Effective contact rate of children  (day^(-1))
+    beta_A = params['beta_A'] # Effective contact rate of adult (day^(-1))
     alpha_H =params['alpha_H'] # Larvae maturity rate (in-human host) (day^(-1))
     n_C = params['n_C'] # Number of Larvae per gram of feces in children (LPG)
     f_C = params['f_C'] # Gram of fecal output per child per day (gram * person^(-1) * (day^(-1)))
@@ -29,7 +29,7 @@ def model(y, t, params):
     f_A = params['f_A'] # Gram of fecal output per adult per day (gram * person^(-1) * (day^(-1)))
     b_D = params['b_D'] # Dog birth rate (day^(-1))
     d_D = params['d_D']  # Dog mortality rate (day^(-1))
-    beta_DB = params['beta_DB'] # Effective transmission rate of dog (day^(-1))
+    beta_DB = params['beta_DB'] # Effective contact rate of dog (day^(-1))
     alpha_D = params['alpha_D'] # Larvae maturity rate (in-Dog host) (day^(-1))
     n_D = params['n_D']  # Number of Larvae per gram of feces in dogs (LPG)
     f_D = params['f_D']  # Gram of fecal output per dog per day (gram * dog^(-1) * (day^(-1)))
@@ -41,8 +41,8 @@ def model(y, t, params):
     # Host population sizes
     Total_Children = S_C + E_C + I_C #Suceptible Children + Exposed Children + Infectious Children
     Total_Adults = S_A + E_A + I_A #Suceptible Adults + Exposed Adults + Infectious Adults
-    Total_Humans = S_C + E_C + I_C + S_A + E_A + I_A #Suceptible Children + Exposed Children + Infectious Children + Suceptible Adults + Exposed Adults + Infectious Adults
-    Total_Dogs = S_D + E_DB + I_DB  #Suceptible Dogs + Exposed Dogs with Non Zoonotic Larvae + Infectious Dogs with Non Zoonotic Larvae 
+    Total_Humans = Total_Children + Total_Adults
+    Total_Dogs = S_D + E_DB + I_DB  #Suceptible Dogs + Exposed Dogs with Lineage B + Infectious Dogs with Lineage B
 
     # Human birth
     human_births = b_H * Total_Humans #Birth rate of Humans * Total Dogs
@@ -58,8 +58,8 @@ def model(y, t, params):
     dog_births = b_D * Total_Dogs #Birth rate of Dogs * Total Dogs
     # Dog death
     dog_deaths_S_D = d_D * S_D #Death rate of Dogs * Suceptible Dogs
-    dog_deaths_E_DB = d_D * E_DB #Death rate of Dogs * Exposed Dogs with Zoonotic Larvae
-    dog_deaths_I_DB = d_D * I_DB #Death rate of Dogs * Infectious Dogs with Zoonotic Larvae
+    dog_deaths_E_DB = d_D * E_DB #Death rate of Dogs * Exposed Dogs with Lineage B
+    dog_deaths_I_DB = d_D * I_DB #Death rate of Dogs * Infectious Dogs with Lineage B
 
     # Ageing from children to adults
     Ageing_S = a * S_C #aging rate from kids to adult * Suceptible Children
@@ -67,13 +67,13 @@ def model(y, t, params):
     Ageing_I = a * I_C #aging rate from kids to adult * Infectious Children
 
     # Larvae death rates in the environment
-    larvae_deaths_env_Z = mu_L * L_Z #Larvae death rate in the environment * Zoonotic Larvae
-    larvae_deaths_env_NZ = mu_L * L_NZ #Larvae death rate in the environment * Non-Zoonotic Larvae
+    larvae_deaths_env_A = mu_L * L_A #Larvae death rate in the environment * Lineage A Larvae
+    larvae_deaths_env_B = mu_L * L_B #Larvae death rate in the environment * Lineage B Larvae
 
     # Force of infection (lambda)
-    lambda_C = beta_C * L_Z / (K + L_Z) #Effective transmission rate of children * Zoonotic Larvae / (CC + Zoonotic Larvae)
-    lambda_A = beta_A * L_Z / (K + L_Z) #Effective transmission rate of adult * Zoonotic Larvae / (CC + Zoonotic Larvae)
-    lambda_DB = beta_DB * L_NZ / (K + L_NZ) #Effective transmission rate of dogs * Zoonotic Larvae / (CC + Zoonotic Larvae)
+    lambda_C = beta_C * L_A / (K + L_A) #Effective transmission rate of children * Lineage A Larvae / (K + Lineage A arvae)
+    lambda_A = beta_A * L_A / (K + L_B) #Effective transmission rate of adult * Lineage A Larvae / (K + Lineage A arvae)
+    lambda_DB = beta_DB * L_NZ / (K + L_NZ) #Effective transmission rate of dogs * Lineage B Larvae / (K + Lineage B arvae)
     
     # Infection rates
     Children_infections = lambda_C * S_C #FoI of children * Susceptible Children
@@ -84,13 +84,12 @@ def model(y, t, params):
     # Progression from exposed to infectious
     infectious_Children = alpha_H * E_C #Larvae maturity rate (in-human host) * Exposed Children
     infectious_Adult = alpha_H * E_A #Larvae maturity rate (in-human host) * Exposed Adults
-    infectious_Dogs_DB = alpha_D * E_DB #Larvae maturity rate (in-dog host) * Exposed dogs with zoonotic larvae
-   
+    infectious_Dogs_DB = alpha_D * E_DB #Larvae maturity rate (in-dog host) * Exposed Dogs with Lineage B
 
     # Contribution to environmental larvae
     human_larvae_output_C = n_C * f_C * I_C #Number of Larvae per gram of feces in children * Gram of fecal output per child per day * Infectious Children
     human_larvae_output_A = n_A * f_A * I_A #Number of Larvae per gram of feces in adults * Gram of fecal output per adult per day * Infectious adult
-    dog_larvae_output_DB = n_D * f_D * I_DB #Number of Larvae per gram of feces in dogs * Gram of fecal output per dog per day * Infectious dog
+    dog_larvae_output_DB = n_D * f_D * I_DB #Number of Larvae per gram of feces in dogs * Gram of fecal output per dog per day * Infectious Dogs with Lineage B
     
     # Differential equations
     dS_C = human_births - Children_infections - Ageing_S - human_deaths_S_C
