850b1187aa29ff8aba3f3e37abfe44cf6ace10f2
Mackrina Winslow <148019291+Mackrina@users.noreply.github.com>
2025-10-23 13:14:23 +1100

    Update filtering logic and output file names

--- Mix/Filter_80_DA.py
+++ Mix/Filter_80_DA.py
@@ -1,35 +1,46 @@
 import pandas as pd
-import numpy as np
-
+import matplotlib.pyplot as plt
 
 # Load CSV file
-df = pd.read_csv('final_derivatives_results_NZ.csv') # upload model outputs
+df = pd.read_csv('final_derivatives_results_Z_and_NZ.csv')
+
 
 # Define the range for final_prevalence_I_C and final_prevalence_I_D
-prevalence_I_C_range = (0.032, 0.21) #95% CI of prevalence in children
-prevalence_I_DB_range = (0.16, 0.43) #95% CI of prevalence in dogs
+prevalence_I_C_range = (0.032, 0.21)
+prevalence_I_DA_80_range =  (0.117,0.366)
+prevalence_I_DB_80_range = (0.0128, 0.168) 
+prevalence_I_D_range = (0.16, 0.43)
 
 # Filter the DataFrame based on the specified ranges
-df_filtered = df[
+filtered_df = df[
     (df['final_prevalence_I_C'] >= prevalence_I_C_range[0]) & 
     (df['final_prevalence_I_C'] <= prevalence_I_C_range[1]) &
     (df['final_prevalence_I_A'] >= 1.7 * df['final_prevalence_I_C']) &
     (df['final_prevalence_I_A'] <= 2.5 * df['final_prevalence_I_C']) &
-    (df['final_prevalence_I_DB'] >= prevalence_I_DB_range[0]) &
-    (df['final_prevalence_I_DB'] <= prevalence_I_DB_range[1])
+    (df['final_prevalence_I_D'] >= prevalence_I_D_range[0]) & 
+    (df['final_prevalence_I_D'] <= prevalence_I_D_range[1]) &
+    #(df['final_prevalence_I_DA'] >= prevalence_I_DA_80_range[0]) &
+    #(df['final_prevalence_I_DA'] <= prevalence_I_DA_80_range[1]) &
+    #(df['final_prevalence_I_DB'] >= prevalence_I_DB_80_range[0]) &
+    #(df['final_prevalence_I_DB'] <= prevalence_I_DB_80_range[1])
+    
+    
 ]
-# Compute I_D/I_C ratio
-df_filtered[ 'ratio'] = df_filtered['final_prevalence_I_DB'] / df_filtered['final_prevalence_I_C']
 
-print('num_rows', len(df_filtered))
+# Compute I_D/I_C ratio
+filtered_df[ 'ratio'] = filtered_df['final_prevalence_I_D'] / filtered_df['final_prevalence_I_C']
+
+print('num_rows', len(filtered_df))
 
 # Apply ratio filter
-df_filtered = df_filtered[(df_filtered['ratio'] >= 1.23) & (df_filtered['ratio'] <= 11.45)]
+filtered_df = filtered_df[(filtered_df['ratio'] >= 1.23) & (filtered_df['ratio'] <=11.45)]
 
 # Print the number of rows after filtering
-print('num_rows', len(df_filtered))
+print('num_rows', len(filtered_df))
+
 
 # Save the filtered DataFrame to a new CSV file
-df_filtered.to_csv('filtered_data_NZ.csv', index=False)
+filtered_df.to_csv('Filetred_80.csv', index=False)
 
-print('Results saved to filtered_data_NZ.csv')
+
+print('results saved to filtered_data.csv')
